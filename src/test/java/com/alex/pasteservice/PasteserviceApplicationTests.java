b57153666be7fa39300bdba54dfd2abe72132a9e
HtonFirst <al.kozlov.rus@gmail.com>
2023-09-09 23:13:24 +0300

    init

---
+++ src/test/java/com/alex/pasteservice/PasteserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.pasteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasteserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
